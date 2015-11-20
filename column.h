2cd2b943330f77bebf6f0efd4bc211c73c40b7cf
Sergey-Shl <Sergeishlyapnikov@gmail.com>
2015-11-20 19:20:37 +0300

    Update column.h

--- column.h
+++ column.h
@@ -1,88 +1,92 @@
-
-
-#include <cassert> // для assert
 #include <iostream>
+#include <iomanip>
 
-#include <iomanip> // для setw
-
+using namespace std;
 
 class column
 {
 private:
-	column *columnPtr;                      // указатель на стек
-	const int size;                   // максимальное количество элементов в стеке
+	vehicle *stackPtr;                      // указатель на стек
+	int size;                   // максимальное количество элементов в стеке
 	int top;                          // номер текущего элемента стека
 public:
 	column(int = 10);                  // по умолчанию размер стека равен 10 элементам
 	column(const column&);          // конструктор копирования
 	~column();                         // деструктор
 
-	inline void push(const column &);     // поместить элемент в вершину стека
-	inline column pop();                   // удалить элемент из вершины стека и вернуть его
-	inline const column &Peek(int) const; // n-й элемент от вершины стека
-	inline int getcolumnSize() const;  // получить размер стека
+	inline void push(const vehicle&);     // поместить элемент в вершину стека
+	inline vehicle pop();                   // удалить элемент из вершины стека и вернуть его
+	inline void printStack();         // вывод стека на экран
+	inline const vehicle &Peek(int) const; // n-й элемент от вершины стека
+	inline int getStackSize() const;  // получить размер стека
+	inline vehicle *getPtr() const;         // получить указатель на стек
 	inline int getTop() const;        // получить номер текущего элемента в стеке
 };
 
-// реализация методов шаблона класса column
-
+// реализация методов шаблона класса STack
 // конструктор Стека
-column::column(int maxSize) :
-size(maxSize) // инициализация константы
+column::column(int maxSize) : size(maxSize) // инициализация константы
 {
-	columnPtr = new column[size]; // выделить память под стек
+	stackPtr = new vehicle[size]; // выделить память под стек
 	top = 0; // инициализируем текущий элемент нулем;
 }
 
 // конструктор копирования
-
-column::column(const column & othercolumn) :
-size(othercolumn.getcolumnSize()) // инициализация константы
+column::column(const column &otherStack) : size(otherStack.getStackSize()) // инициализация константы
 {
-	columnPtr = new column[size]; // выделить память под новый стек
-	top = othercolumn.getTop();
+	stackPtr = new vehicle[size]; // выделить память под новый стек
+	top = otherStack.getTop();
 
 	for (int ix = 0; ix < top; ix++)
-		columnPtr[ix] = othercolumn.getPtr()[ix];
+		stackPtr[ix] = otherStack.getPtr()[ix];
 }
 
 // функция деструктора Стека
 column::~column()
 {
-	delete[] columnPtr; // удаляем стек
+	delete[] stackPtr; // удаляем стек
 }
 
 // функция добавления элемента в стек
-inline void column::push(const column &value)
+inline void column::push(const vehicle &value)
 {
 	// проверяем размер стека
-	assert(top < size); // номер текущего элемента должен быть меньше размера стека
-
-	columnPtr[top++] = value; // помещаем элемент в стек
+	if(top < size) // номер текущего элемента должен быть меньше размера стека
+		stackPtr[top++] = value; // помещаем элемент в стек
 }
 
 // функция удаления элемента из стека
-inline column column::pop()
+inline vehicle column::pop()
 {
 	// проверяем размер стека
-	assert(top > 0); // номер текущего элемента должен быть больше 0
-
-	columnPtr[--top]; // удаляем элемент из стека
+	if(top > 0) // номер текущего элемента должен быть больше 0
+		stackPtr[--top]; // удаляем элемент из стека
 }
 
 // функция возвращает n-й элемент от вершины стека
-inline const column &column::Peek(int nom) const
+inline const vehicle& column::Peek(int nom) const
 {
-	//
-	assert(nom <= top);
+	if(nom <= top)
+		return stackPtr[top - nom]; // вернуть n-й элемент стека
+}
 
-	return columnPtr[top - nom]; // вернуть n-й элемент стека
+// вывод стека на экран
+inline void column::printStack()
+{
+	for (int ix = top - 1; ix >= 0; ix--)
+		cout << "|" << setw(4) << stackPtr[ix].idVehicle << endl;
 }
 
 // вернуть размер стека
-inline int column::getcolumnSize() const
+inline int column::getStackSize() const
 {
 	return size;
+}
+
+// вернуть указатель на стек (для конструктора копирования)
+inline vehicle* column::getPtr() const
+{
+	return stackPtr;
 }
 
 // вернуть размер стека
