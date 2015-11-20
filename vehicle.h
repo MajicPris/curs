59c63de9a81f607edef2205657cc64d9407719ea
Sergey-Shl <Sergeishlyapnikov@gmail.com>
2015-11-20 19:20:00 +0300

    Update and rename car.h to vehicle.h

--- vehicle.h
+++ vehicle.h
@@ -1,10 +1,9 @@
 #include <glm\glm.hpp>
 #include <glm\ext.hpp> 
+
 using namespace std;
 
-
 class vehicle {
-
 public:
 	float x, y;
 	glm::vec3 speed;
@@ -19,9 +18,10 @@ public:
 
 };
 
-vehicle::vehicle(){
+vehicle::vehicle()
+{
 	x = 0.0, y = 0.0;
-	speed = {0.0, 0.0, 0.0}; //glm::vec3(0.0, 0.0, 0.0)
+	speed = { 0.0, 0.0, 0.0 }; //glm::vec3(0.0, 0.0, 0.0)
 	acceleration = 0.0, deceleration = 0.0;
 	width = 0.0, height = 0.0;
 	weight = 0.0;
@@ -29,7 +29,8 @@ vehicle::vehicle(){
 	position = 0;
 }
 
-vehicle::vehicle(float xx, float yy, glm::vec3 s, float a, float d, float w, float h, float weig, int i, int p){
+vehicle::vehicle(float xx, float yy, glm::vec3 s, float a, float d, float w, float h, float weig, int i, int p)
+{
 	x = xx, y = yy;
 	speed = s; //glm::vec3(0.0, 0.0, 0.0)
 	acceleration = a, deceleration = d;
