8dce233f557274c1e27ca258860435905aad8a97
Konstantin <ksharutin@gmail.com>
2016-02-29 20:25:01 +0300

    Update vehicle.h

--- vehicle.h
+++ vehicle.h
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+class steering_wheel
+{
+public:
+
+
+};
+
 class vehicle {
 public:
 	float x, y;
@@ -17,13 +24,24 @@ public:
 	int position;
 
 
+
 	vehicle();
 	vehicle(float, float, float, glm::vec3, float, float, float, float, float, int, int);
 
 	float getAngle();
+	float getAngle(glm::vec2 vec);
 	void checkSpeed(vehicle v1);
 	void checkDis(vehicle v1);
-	void vehicle::rotate(float angle);
+	void rotate(float angle);
+
+
+	float angle_of_wheel;
+	float angle_of_rotate;
+	float common_angle;
+	glm::vec3 old_dir;
+	bool rotated;
+	bool busy;
+	void rotate_the_car();
 };
 
 glm::vec3 normolize(glm::vec3);
@@ -40,6 +58,10 @@ vehicle::vehicle()
 	weight = 0.0;
 	idVehicle = 0;
 	position = 0;
+
+	angle_of_wheel = 0;
+	old_dir = { 0.0, 0.0, 0.0 };
+	rotated = true;
 
 	float w2 = width / 2;
 	float h2 = height / 2;
@@ -112,6 +134,14 @@ float vehicle::getAngle()
 	return -f;
 }
 
+float vehicle::getAngle(glm::vec2 vec)
+{
+	float f = glm::orientedAngle(glm::vec2(direction.x, direction.y), normolize(vec));
+	//float f = glm::angle(direction, normolize(glm::vec3(((xCoord[0] + xCoord[1]) / 2) - x, ((yCoord[0] + yCoord[1]) / 2) - y, 0.0)));
+	return -f;
+}
+
+
 void vehicle::checkDis(vehicle v1)
 {
 	float dis = sqrt(pow((this->x - v1.x), 2) + pow((this->y - v1.y), 2));
@@ -136,9 +166,48 @@ void vehicle::checkSpeed(vehicle v1)
 }
 
 void vehicle::rotate(float angle){
+	float x1, y1;
 	for (int j = 0; j < 4; j++)
 	{
-		xCoord[j] = this->x + (xCoord[j] - this->x) * cos(angle) - (yCoord[j] - this->y) * sin(angle);
-		yCoord[j] = this->y + (yCoord[j] - this->y) * cos(angle) + (xCoord[j] - this->x) * sin(angle);
+		x1 = this->x + (xCoord[j] - this->x) * cos(angle) - (yCoord[j] - this->y) * sin(angle);
+		y1 = this->y + (yCoord[j] - this->y) * cos(angle) + (xCoord[j] - this->x) * sin(angle);
+		xCoord[j] = x1;
+		yCoord[j] = y1;
 	}
+}
+
+void vehicle::rotate_the_car(){
+	if (abs(this->common_angle) < abs(this->angle_of_rotate))
+	{
+		float x1, y1;
+		float angle = angle_of_wheel;
+		float a = (abs(this->angle_of_rotate) - abs(this->common_angle));
+		if (a < abs(this->angle_of_wheel))
+		{
+			if (angle_of_wheel < 0)
+				angle = -a;
+			else
+				angle = -a;
+			this->rotated = true;
+		}
+		this->common_angle += angle;
+		x1 = (this->direction.x) * cos(angle) - (this->direction.y) * sin(angle);
+		y1 = (this->direction.y) * cos(angle) + (this->direction.x) * sin(angle);
+		this->direction.x = x1;
+		this->direction.y = y1;
+		for (int j = 0; j < 4; j++)
+		{
+			x1 = this->x + (xCoord[j] - this->x) * cos(angle) - (yCoord[j] - this->y) * sin(angle);
+			y1 = this->y + (yCoord[j] - this->y) * cos(angle) + (xCoord[j] - this->x) * sin(angle);
+			xCoord[j] = x1;
+			yCoord[j] = y1;
+		}
+	}
+ else
+ {
+	 this->common_angle = 0;
+	 this->rotated = true;
+ }
+
+
 }
